05a95cf8ef1ea4d8007d03f22d6785eacf0e4435
Allen Zhang <azhang9328@gmail.com>
2019-11-17 04:03:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
   monopoly = {:railroads => 
     {:pieces => 4 }
   }
-  monopoly[:railroads] = []
+  monopoly[:railroads] = :names
   monopoly[:railroads][:rent_in_dollars] = {
     :key1 => 1, 
     :key2 => 2, 
